654cec43ce9c91774cce0b5f22f07cdfd0670b88
bozhink <bozhink@gmail.com>
2015-10-23 23:08:15 +0300

    EntityFramework Code First homework

---
+++ Databases - 2015/Homeworks/DatabasesHomeworks/13. EntityFrameworkCodeFirst/StudentSystem.Models/Homework.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentSystem.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Homework
+    {
+        public int HomeworkId { get; set; }
+
+        [MaxLength(100)]
+        public string Content { get; set; }
+
+        public DateTime TimeSent { get; set; }
+    }
+}
