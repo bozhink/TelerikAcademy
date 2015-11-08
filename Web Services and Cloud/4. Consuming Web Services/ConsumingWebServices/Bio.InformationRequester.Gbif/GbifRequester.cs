96997791ab16941a6aeeddb965755bea7277c352
bozhink <bozhink@gmail.com>
2015-11-08 21:42:04 +0200

    Cunsuming Web Services update

---
+++ Web Services and Cloud/4. Consuming Web Services/ConsumingWebServices/Bio.InformationRequester.Gbif/GbifRequester.cs
@@ -0,0 +1,34 @@
+﻿namespace Bio.InformationRequester.Gbif
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Models;
+    using Newtonsoft.Json;
+
+    public class GbifRequester
+    {
+        public async Task<GbifResult> GetSpeciesInformation(string scientificName)
+        {
+            if (string.IsNullOrEmpty(scientificName.Trim()))
+            {
+                throw new ArgumentNullException("scientificName");
+            }
+
+            string requestName = Uri.EscapeDataString(Uri.UnescapeDataString(scientificName))
+                .Replace("%20", "+");
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.gbif.org");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var resposeJsonString = await client.GetStringAsync($"v0.9/species/match?verbose=true&name={requestName}");
+
+                var response = JsonConvert.DeserializeObject<GbifResult>(resposeJsonString);
+                return response;
+            }
+        }
+    }
+}
---
+++ Web Services and Cloud/4. Consuming Web Services/ConsumingWebServices/Bio.InformationRequester.Gbif/Models/Alternative.cs
@@ -0,0 +1,68 @@
+﻿namespace Bio.InformationRequester.Gbif.Models
+{
+    using Newtonsoft.Json;
+
+    [JsonObject]
+    public class Alternative
+    {
+       [JsonProperty(propertyName: "usageKey")]
+        public int UsageKey { get; set; }
+
+       [JsonProperty(propertyName: "scientificName")]
+        public string ScientificName { get; set; }
+
+       [JsonProperty(propertyName: "canonicalName")]
+        public string CanonicalName { get; set; }
+
+       [JsonProperty(propertyName: "rank")]
+        public string Rank { get; set; }
+
+       [JsonProperty(propertyName: "synonym")]
+        public bool Synonym { get; set; }
+
+       [JsonProperty(propertyName: "confidence")]
+        public int Confidence { get; set; }
+
+       [JsonProperty(propertyName: "note")]
+        public string Note { get; set; }
+
+       [JsonProperty(propertyName: "matchType")]
+        public string MatchType { get; set; }
+
+       [JsonProperty(propertyName: "kingdom")]
+        public string Kingdom { get; set; }
+
+       [JsonProperty(propertyName: "phylum")]
+        public string Phylum { get; set; }
+
+       [JsonProperty(propertyName: "order")]
+        public string Order { get; set; }
+
+       [JsonProperty(propertyName: "family")]
+        public string Family { get; set; }
+
+       [JsonProperty(propertyName: "genus")]
+        public string Genus { get; set; }
+
+       [JsonProperty(propertyName: "kingdomKey")]
+        public int KingdomKey { get; set; }
+
+       [JsonProperty(propertyName: "phylumKey")]
+        public int PhylumKey { get; set; }
+
+       [JsonProperty(propertyName: "classKey")]
+        public int ClassKey { get; set; }
+
+       [JsonProperty(propertyName: "orderKey")]
+        public int OrderKey { get; set; }
+
+       [JsonProperty(propertyName: "familyKey")]
+        public int FamilyKey { get; set; }
+
+       [JsonProperty(propertyName: "genusKey")]
+        public int GenusKey { get; set; }
+
+       [JsonProperty(propertyName: "class")]
+        public string Class { get; set; }
+    }
+}
