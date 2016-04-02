eee8f1f0b47dfa14f2fbb80535ef81f7c8f0370f
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-02 21:09:03 +0300

    Age is added.

---
+++ Age/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Age
+{
+    using System;
+    using System.Globalization;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CultureInfo us = new CultureInfo("en-US");
+
+            string birthdayString = Console.ReadLine();
+
+            DateTime birthday;
+            if (!DateTime.TryParse(birthdayString, us, DateTimeStyles.None, out birthday))
+            {
+                return;
+            }
+
+            var now = DateTime.Now;
+            var currentAge = now - birthday;
+
+            var zero = new TimeSpan(0);
+            if (currentAge < zero)
+            {
+                currentAge = zero;
+            }
+
+            int age = currentAge.Days / 365;
+
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+        }
+    }
+}
