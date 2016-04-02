ca6cc2d4497f0aa088495b172b5e33325ea30b19
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-02 20:44:39 +0300

    PrintSequence is added.

---
+++ PrintSequence/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PrintSequence
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int NumberOfItemsToPrint = 10;
+            const int InitialNumber = 2;
+
+            int sign = 1;
+            int currentNumber = InitialNumber;
+            for (int i = 0; i < NumberOfItemsToPrint; ++i)
+            {
+                Console.WriteLine(sign * currentNumber);
+                ++currentNumber;
+                sign *= -1;
+            }
+        }
+    }
+}
