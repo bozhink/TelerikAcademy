054619adb05dea249831e62d3ad02fc0b68eec20
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-10 15:46:29 +0300

    Added Exchange Variable Values to CSharp-Part-1

---
+++ CSharp-1 - 2016/Homeworks/02-Types-and-Variables/ExchangeVaraibleValues/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExchangeVaraibleValues
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int a = 5;
+			int b = 10;
+			Console.WriteLine ("{0} {1}", a, b);
+
+			int swap = a;
+			a = b;
+			b = swap;
+			Console.WriteLine ("{0} {1}", a, b);
+		}
+	}
+}
