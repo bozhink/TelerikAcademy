7ed31610b9b683408fb7a29e0f39ee4710dfaed2
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-10 16:14:18 +0300

    Added PrintTheAsciiTable to CSharp-Part-1;

---
+++ CSharp-1 - 2016/Homeworks/02-Types-and-Variables/PrintTheAsciiTable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PrintTheAsciiTable
+{
+	using System;
+	using System.Text;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var stringBuilder = new StringBuilder ();
+
+			for (int i = 33; i <= 126; ++i) {
+				stringBuilder.Append ((char)i);
+			}
+
+			var result = stringBuilder.ToString ();
+			if (result.Length != 94) {
+				throw new ApplicationException ();
+			} else {
+				Console.WriteLine (result);
+			}
+		}
+	}
+}
