6d16cbdaf9a05a0863907edc3d6210a8b3ffe1be
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-10 15:28:43 +0300

    Added Strings and Objects in CSharp-PArt-1

---
+++ CSharp-1 - 2016/Homeworks/02-Types-and-Variables/StringsAndObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringsAndObjects
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string hello = "Hello";
+			string world = "World";
+			object helloWorld = hello + " " + world;
+			string message = (string)helloWorld;
+			Console.WriteLine (message);
+		}
+	}
+}
