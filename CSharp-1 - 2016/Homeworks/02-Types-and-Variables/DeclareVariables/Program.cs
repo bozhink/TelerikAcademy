b282915619f7bc7699768d4e5d91f4e07494b205
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-10 15:03:08 +0300

    Added DeclareVariables in CSharp-1, homework 2.

---
+++ CSharp-1 - 2016/Homeworks/02-Types-and-Variables/DeclareVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DeclareVariables
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			byte var1 = 97;
+			Console.WriteLine (var1);
+
+			sbyte var2 = -115;
+			Console.WriteLine (var2);
+
+			short var3 = -10000;
+			Console.WriteLine (var3);
+
+			uint var4 = 52130;
+			Console.WriteLine (var4);
+
+			ulong var5 = 4825932L;
+			Console.WriteLine (var5);
+		}
+	}
+}
