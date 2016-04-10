f807829e4d4c657025432dbc7b136fbfa01fd4fb
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-10 15:24:07 +0300

    Added Boolean Variable in CSharp-Part-1

---
+++ CSharp-1 - 2016/Homeworks/02-Types-and-Variables/BooleanVariable/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BooleanVariable
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			bool isFemale = false;
+			Console.WriteLine (isFemale);
+		}
+	}
+}
