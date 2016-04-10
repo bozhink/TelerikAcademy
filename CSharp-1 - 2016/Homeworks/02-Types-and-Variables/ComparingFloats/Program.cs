da1c4864b2b76f2da49692dc6ba5e55301a366c1
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-10 16:00:47 +0300

    Added Comparing Floats to CSharp-Part-1

---
+++ CSharp-1 - 2016/Homeworks/02-Types-and-Variables/ComparingFloats/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ComparingFloats
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double a = double.Parse (Console.ReadLine ());
+			double b = double.Parse (Console.ReadLine ());
+
+			double eps = 0.000001;
+			Console.WriteLine ((Math.Abs (a - b) < eps).ToString().ToLower());
+		}
+	}
+}
