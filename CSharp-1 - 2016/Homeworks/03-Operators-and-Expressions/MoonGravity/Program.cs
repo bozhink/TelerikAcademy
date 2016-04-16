40d7dfaa1dddb68b30eb23333754be72da6838a1
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-16 10:17:47 +0300

    CSharp-Part-1: 03-Operators-and-Expressions is added

---
+++ CSharp-1 - 2016/Homeworks/03-Operators-and-Expressions/MoonGravity/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MoonGravity
+{
+	using System;
+
+	public class MainClass
+	{
+		private const double MoonToEarthGravityRatio = 0.17;
+		private const string OutputFormat = "f3";
+
+		public static void Main (string[] args)
+		{
+			double weightOnEarth = double.Parse (Console.ReadLine ());
+			double weightOnMoon = MoonToEarthGravityRatio * weightOnEarth;
+
+			Console.WriteLine (weightOnMoon.ToString(OutputFormat));
+		}
+	}
+}
---
+++ CSharp-1 - 2016/Homeworks/03-Operators-and-Expressions/OddOrEven/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OddOrEven
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int input = int.Parse (Console.ReadLine ());
+
+			if (2 * (input / 2) == input) {
+				Console.WriteLine ("even {0}", input);
+			} else {
+				Console.WriteLine ("odd {0}", input);
+			}
+		}
+	}
+}
