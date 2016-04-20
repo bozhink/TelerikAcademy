93249023c91a748b64e0db959bb2ce4dcfba0b6b
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-20 21:20:16 +0300

    ExchangeIfGreater added to CSharp-1 - hw-05

---
+++ CSharp-1 - 2016/Homeworks/05-Conditional-Statements/ExchangeIfGreater/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ExchangeIfGreater
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double a = double.Parse (Console.ReadLine ());
+			double b = double.Parse (Console.ReadLine ());
+
+			double swap;
+
+			if (a > b) {
+				swap = b;
+				b = a;
+				a = swap;
+			}
+
+			Console.WriteLine ("{0} {1}", a, b);
+		}
+	}
+}
