5ef849999e0fd3a622c2a7e85ed0efff7777d2d4
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-20 22:00:04 +0300

    BiggestOf5 is added to CSharp-1 - hw-05

---
+++ CSharp-1 - 2016/Homeworks/05-Conditional-Statements/BiggestOf5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BiggestOf5
+{
+	using System;
+
+	public class MainClass
+	{
+		private const int NumberOfItemsToRead = 5;
+
+		public static void Main (string[] args)
+		{
+			double[] inputs = new double[NumberOfItemsToRead];
+
+			for (int i = 0; i < NumberOfItemsToRead; ++i) {
+				inputs [i] = double.Parse (Console.ReadLine ());
+			}
+
+			double biggest = inputs [0];
+			for (int i = 1; i < NumberOfItemsToRead; ++i) {
+				if (biggest < inputs [i]) {
+					biggest = inputs [i];
+				}
+			}
+
+			Console.WriteLine (biggest);
+		}
+	}
+}
