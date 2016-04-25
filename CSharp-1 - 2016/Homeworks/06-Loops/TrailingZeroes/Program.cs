9347a3072ae60e057da13ba3edc51227cdd6e1f1
Bozhin Karaivanov <bozhink@gmail.com>
2016-04-26 00:08:20 +0300

    TrailingZeroes is added.

---
+++ CSharp-1 - 2016/Homeworks/06-Loops/TrailingZeroes/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TrailingZeroes
+{
+    using System;
+    using System.Numerics;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = 0; i < n;)
+            {
+                factorial *= ++i;
+            }
+
+            string stringValue = factorial.ToString();
+            string trailingZeroes = Regex.Match(stringValue, @"0+\Z").Value;
+
+            Console.WriteLine(trailingZeroes.Length);
+        }
+    }
+}
