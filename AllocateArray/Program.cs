c499fdf9dd538997ab7b2e8416992ee67c4bf2bd
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-08 22:32:20 +0300

    AllocateArray is implemented.

---
+++ AllocateArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AllocateArray
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = i * 5;
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
