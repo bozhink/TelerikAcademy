a3a6f271dced656821f4e494fabc134a6c6a14f4
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-09 23:45:31 +0300

    MaximalIncreasingSequence is added.

---
+++ MaximalIncreasingSequence/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MaximalIncreasingSequence
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 1)
+            {
+                return;
+            }
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maximalSequenceLength = 1;
+            int sequenceLength = 1;
+            for (int i = 1; i < n; ++i)
+            {
+                if (array[i] == array[i-1] + 1)
+                {
+                    sequenceLength++;
+                }
+                else
+                {
+                    if (maximalSequenceLength < sequenceLength)
+                    {
+                        maximalSequenceLength = sequenceLength;
+                    }
+
+                    sequenceLength = 1;
+                }
+            }
+
+            Console.WriteLine(maximalSequenceLength);
+        }
+    }
+}
