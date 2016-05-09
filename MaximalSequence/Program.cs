33c7d470096de82be940c9c9af7cc3c3439ba9c8
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-09 23:36:50 +0300

    MaximalSequence is added.

---
+++ MaximalSequence/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MaximalSequence
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < 1)
+            {
+                return;
+            }
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maximalSequenceLength = 1;
+            int sequenceLength = 1;
+            int sequenceInteger = array[0];
+            for (int i = 1; i < n; ++i)
+            {
+                if (array[i] == sequenceInteger)
+                {
+                    sequenceLength++;
+                }
+                else
+                {
+                    sequenceInteger = array[i];
+                    if (maximalSequenceLength < sequenceLength)
+                    {
+                        maximalSequenceLength = sequenceLength;
+                    }
+
+                    sequenceLength = 1;
+                }
+            }
+
+            Console.WriteLine(maximalSequenceLength);
+        }
+    }
+}
