d9d5a500eb252fa343916dfd72c6f312c1a582ae
bozhink <bozhink@gmail.com>
2016-05-10 16:19:42 +0300

    PrimeNumbers is implemented.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PrimeNumbers
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            uint n = uint.Parse(Console.ReadLine());
+            bool result = false;
+            if (!IsPrime(n))
+            {
+                do
+                {
+                    n--;
+                    result = IsPrime(n);
+
+                } while (!result);
+            }
+
+            Console.WriteLine(n);
+        }
+
+        private static bool IsPrime(uint n)
+        {
+            for (uint i = 2; i <= Math.Sqrt(n) + 1; ++i)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
