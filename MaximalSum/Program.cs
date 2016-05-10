194a53ae85e23af021e97fb35f9fd1e85c7c7235
bozhink <bozhink@gmail.com>
2016-05-10 15:32:47 +0300

    MaximalSum is implemented.

---
+++ MaximalSum/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MaximalSum
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maximalSum = array[0];
+            int currentMaximalSum = array[0];
+            for (int i = 1; i < n; ++i)
+            {
+                currentMaximalSum = Math.Max(array[i], currentMaximalSum + array[i]);
+                maximalSum = Math.Max(currentMaximalSum, maximalSum);
+            }
+
+            Console.WriteLine(maximalSum);
+        }
+    }
+}
