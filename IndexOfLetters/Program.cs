cdeb89a8036c7d77b0b9349d18f0a5939d9a9187
bozhink <bozhink@gmail.com>
2016-05-10 16:07:23 +0300

    IndexOfLetters is implemented.

---
+++ IndexOfLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace IndexOfLetters
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[] array = new char[122 - 96];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (char)(97 + i);
+            }
+
+            int n = array.Length;
+
+            string word = Console.ReadLine();
+            foreach (char ch in word)
+            {
+                for (int i = 0; i < n; ++i)
+                {
+                    if (ch == array[i])
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
