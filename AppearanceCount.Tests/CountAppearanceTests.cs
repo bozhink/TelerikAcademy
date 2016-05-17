3b6beed293ddf42555e85a851671e2f29c37be71
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-17 22:15:31 +0300

    AppearanceCount is implemented.

---
+++ AppearanceCount.Tests/CountAppearanceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace AppearanceCount.Tests
+{
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CountAppearanceTests
+    {
+        [TestMethod]
+        public void CountAppearance_WithZeroTestData_ShouldWork()
+        {
+            int n = int.Parse("8");
+            int[] array = "28 6 21 6 -7856 73 73 -56".Split(' ').Select(s => int.Parse(s)).ToArray();
+            int x = int.Parse("73");
+
+            Assert.AreEqual(2, AppearanceCount.Program.CountAppearance(x, array), "Appearance count should be 2.");
+        }
+    }
+}
---
+++ AppearanceCount/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace AppearanceCount
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] array = input.Select(i => int.Parse(i)).ToArray();
+
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountAppearance(x, array));
+        }
+
+        public static int CountAppearance(int x, int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == x)
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+    }
+}
