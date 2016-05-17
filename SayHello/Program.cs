8742929719418f4830b6929e54117868a08ab9e7
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-17 21:30:22 +0300

    SayHello is implemented.

---
+++ SayHello/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SayHello
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SayHello(Console.ReadLine());
+        }
+
+        public static void SayHello(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
