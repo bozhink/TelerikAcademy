5e93703850eda6e9d13019675969aea4a030ac15
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-17 21:35:53 +0300

    GetLargestNumber is implemented.

---
+++ GetLargestNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GetLargestNumber
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int max01 = GetMax(int.Parse(input[0]), int.Parse(input[1]));
+            Console.WriteLine(GetMax(max01, int.Parse(input[2])));
+        }
+
+        public static int GetMax(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+    }
+}
