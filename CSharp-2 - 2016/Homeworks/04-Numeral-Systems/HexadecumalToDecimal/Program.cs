b97b325dbd194bfeb8f2d8c53caec2957c0c91d3
bozhink <bozhink@gmail.com>
2016-05-18 10:43:54 +0300

    04: DecimalToHexadecimal and HexadecimalToDecimal are implemented.

---
+++ CSharp-2 - 2016/Homeworks/04-Numeral-Systems/HexadecumalToDecimal/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HexadecumalToDecimal
+{
+    using System;
+    using System.Text;
+    
+    public class Program
+    {
+        private const int NumericalBase = 16;
+        private const string Alphabet = "0123456789ABCDEF";
+        
+        public static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            Console.WriteLine(HexadecumalToDecimal(n));
+        }
+        
+        public static ulong HexadecumalToDecimal(string n)
+        {
+            ulong result = 0;
+            ulong multiplier = 1;
+            
+            for (int i = n.Length - 1; i >= 0; --i)
+            {
+                char digit = n[i];
+                result += multiplier * (ulong)Alphabet.IndexOf(digit);
+                multiplier *= NumericalBase;
+            }
+            
+            return result;
+        }
+    }
+}
