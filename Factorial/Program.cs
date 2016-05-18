de2abbbf57ca17819b31c57a17b68353099de459
bozhink <bozhink@gmail.com>
2016-05-18 15:50:56 +0300

    Factorial is implemented.

---
+++ Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Factorial
+{
+    using System;
+    using System.Numerics;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] array = new int[number];
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i + 1;
+            }
+
+            Console.WriteLine(CalculateFactorial(array));
+        }
+
+        public static BigInteger CalculateFactorial(int[] array)
+        {
+            BigInteger result = 1;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                result *= array[i];
+            }
+
+            return result;
+        }
+    }
+}
