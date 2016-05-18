482f37222cd953df5689014ab5bb7b6b2ed24fbe
bozhink <bozhink@gmail.com>
2016-05-18 10:18:28 +0300

    04: DecimalToBinary is implemented.

---
+++ CSharp-2 - 2016/Homeworks/04-Numeral-Systems/DecimalToBinary/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DecimalToBinary
+{
+    using System;
+    using System.Text;
+    
+    public class Program
+    {
+        private const int NumericalBase = 2;
+        
+        public static void Main(string[] args)
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+            Console.WriteLine(DecimalToBinary(n));
+        }
+        
+        public static string DecimalToBinary(ulong n)
+        {
+            ulong x = n;
+            var stringBuilder = new StringBuilder();
+
+            while (x > 0)
+            {
+                stringBuilder.Insert(0, (x % NumericalBase).ToString());
+                x /= NumericalBase;
+            }
+            
+            return stringBuilder.ToString();
+        }
+    }
+}
