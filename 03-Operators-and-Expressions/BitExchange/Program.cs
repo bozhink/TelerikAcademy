496cd4edbcf9f50446eca4664dc4377b3dc3879d
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-23 23:31:44 +0300

    Files are moved to 03-Operators-and-Expressions.

---
+++ 03-Operators-and-Expressions/BitExchange/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace BitExchange
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            uint n = uint.Parse(Console.ReadLine());
+
+            uint bit3 = (n >> 3) & 1u;
+            uint bit4 = (n >> 4) & 1u;
+            uint bit5 = (n >> 5) & 1u;
+
+            uint bit24 = (n >> 24) & 1u;
+            uint bit25 = (n >> 25) & 1u;
+            uint bit26 = (n >> 26) & 1u;
+
+            n = ChangeBit(n, (byte)bit3, 24);
+            n = ChangeBit(n, (byte)bit24, 3);
+
+            n = ChangeBit(n, (byte)bit4, 25);
+            n = ChangeBit(n, (byte)bit25, 4);
+
+            n = ChangeBit(n, (byte)bit5, 26);
+            n = ChangeBit(n, (byte)bit26, 5);
+
+            Console.WriteLine(n);
+
+        }
+
+        public static uint ChangeBit(uint number, byte value, byte position)
+        {
+            switch (value)
+            {
+                case 0:
+                    return number & (~Mask(position));
+
+                case 1:
+                    return number | Mask(position);
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public static uint Mask(byte position)
+        {
+            uint mask = 1u;
+            mask <<= position;
+            return mask;
+        }
+    }
+}
---
+++ 03-Operators-and-Expressions/ModifyBit/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ModifyBit
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+            byte p = byte.Parse(Console.ReadLine());
+            byte v = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine(ChangeBit(n, v, p));
+        }
+
+        public static ulong ChangeBit(ulong number, byte value, byte position)
+        {
+            switch (value)
+            {
+                case 0:
+                    return number & (~Mask(position));
+
+                case 1:
+                    return number | Mask(position);
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public static ulong Mask(byte position)
+        {
+            ulong mask = 1u;
+            mask <<= position;
+            return mask;
+        }
+    }
+}
