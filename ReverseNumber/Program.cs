719511ae1bfdf584c44e5bcd4278f0c9ce36ae20
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-23 23:50:19 +0300

    Unknown commit status.

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ReverseNumber
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            decimal x = decimal.Parse(Console.ReadLine());
+        }
+    }
+}
