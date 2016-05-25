9295d973f63ef452bc8a6ae2d3841ae3e0d8ba7e
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-25 22:17:39 +0300

    CorrectBrackets is in 77/100.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/CorrectBrackets/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CorrectBrackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        private const string Incorrect = "Incorrect";
+        private const string Correct = "Correct";
+
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var stack = new Stack<char>();
+            for (int i = 0; i < input.Length; ++i)
+            {
+                char ch = input[i];
+                switch (ch)
+                {
+                    case '(':
+                        stack.Push(ch);
+                        break;
+
+                    case ')':
+                        if (stack.Count > 0)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            Console.WriteLine(Incorrect);
+                            return;
+                        }
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(Correct);
+        }
+    }
+}
