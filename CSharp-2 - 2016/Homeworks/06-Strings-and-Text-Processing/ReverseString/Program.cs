8ec37d38b175d72bac586c6a39966733b84b3f67
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-25 21:53:58 +0300

    ReverseString is implemented.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseString
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var stringBuilder = new StringBuilder();
+
+            for (int i = input.Length - 1; i >= 0; --i)
+            {
+                stringBuilder.Append(input[i]);
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
