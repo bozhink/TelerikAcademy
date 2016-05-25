0acabb3626e4d6274f8ed6d78551484faa3e7ae6
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-25 22:34:59 +0300

    SubstringInText is implemented.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/SubstringInText/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SubstringInText
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            Console.WriteLine(CountNumberOfOcurrences(text, pattern));
+        }
+
+        public static int CountNumberOfOcurrences(string text, string pattern)
+        {
+            int result = 0;
+
+            int index = -1;
+
+            for (int i = 0; i < text.Length; ++i)
+            {
+                index = text.IndexOf(pattern, index + 1, StringComparison.InvariantCultureIgnoreCase);
+
+                if (index >= 0)
+                {
+                    ++result;
+                    i = index;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
