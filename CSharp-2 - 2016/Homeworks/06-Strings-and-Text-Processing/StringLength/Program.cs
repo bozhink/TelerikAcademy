593c3d29ce737e92e8235a383c814cbe2c97ad55
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-25 23:22:17 +0300

    StringLength is implemented.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/StringLength/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringLength
+{
+    using System;
+
+    public class Program
+    {
+        private const int MaximalLength = 20;
+        private const char FillingChar = '*';
+
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Replace(@"\", string.Empty);
+            Console.WriteLine(input.PadRight(MaximalLength, FillingChar));
+        }
+    }
+}
