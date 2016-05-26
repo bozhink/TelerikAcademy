f64348d2ef22eb718421f5a87c3407f0ade7e8a8
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-26 23:49:40 +0300

    SequenceOfLetters is implemented.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/SequenceOfLetters/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SequenceOfLetters
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(ReplaceSequentalLetter(input));
+        }
+
+        public static string ReplaceSequentalLetter(string input)
+        {
+            if (input.Length < 1)
+            {
+                return input;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            char lastCharacter = input[0];
+            stringBuilder.Append(lastCharacter);
+
+            for (int i = 1; i < input.Length; ++i)
+            {
+                if (input[i] == lastCharacter)
+                {
+                    continue;
+                }
+
+                lastCharacter = input[i];
+                stringBuilder.Append(lastCharacter);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
