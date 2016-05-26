ed7551cd1dae968f5394e3978c9997fa9b85591e
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-26 23:10:10 +0300

    UnicodeCharacters is initialized.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/UnicodeCharacters/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace UnicodeCharacters
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(GetUnicodeString(input));
+        }
+
+        public static string GetUnicodeString(string s)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < s.Length; ++i)
+            {
+                char c = s[i];
+                sb.Append(@"\u");
+                sb.Append(string.Format("{0:X4}", (int)c));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
