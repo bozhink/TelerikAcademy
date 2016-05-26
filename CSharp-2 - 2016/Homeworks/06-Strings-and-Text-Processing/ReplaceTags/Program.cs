275987217820e9de03025a4db991d52552a3e0d5
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-26 23:41:39 +0300

    ReplaceTags is initialized.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/ReplaceTags/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ReplaceTags
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Regex matchAnchor = new Regex(@"<a\b[^>]+href=""([^""<>]+)""[^>]*>([\s\S]*?)</a>");
+
+            string result = matchAnchor.Replace(input, "[$2]($1)");
+
+            Console.WriteLine(result);
+        }
+    }
+}
