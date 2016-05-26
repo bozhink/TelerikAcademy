27bf24fe8f65887bc767b7d20c2ba7829b862304
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-26 23:21:08 +0300

    ParseUrl is implemented.

---
+++ CSharp-2 - 2016/Homeworks/06-Strings-and-Text-Processing/ParseUrl/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ParseUrl
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Regex matchUrl = new Regex(@"(?<protocol>[A-Za-z]+)://(?<server>[^/]+)(?<resource>/.*)");
+
+            var parts = matchUrl.Match(input);
+            var protocol = parts.Groups["protocol"].Value;
+            var server = parts.Groups["server"].Value;
+            var resource = parts.Groups["resource"].Value;
+
+            Console.WriteLine("[protocol] = {0}", protocol);
+            Console.WriteLine("[server] = {0}", server);
+            Console.WriteLine("[resource] = {0}", resource);
+        }
+    }
+}
