9e1a365c69ef81f2035ef1c486ba2ac13cc81695
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-29 17:21:41 +0300

    CSharp-2: Homework 07 Exception Handling is implemented.

---
+++ CSharp-2 - 2016/Homeworks/07-Exception-Handling/07-Exception-Hadling/EnterNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace EnterNumbers
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class MainClass
+	{
+		private const int NumberOfExpectedNumbers = 10;
+		private const int MinimalNumber = 1;
+		private const int MaximalNumber = 100;
+
+		private static readonly List<int> Numbers = new List<int> ();
+
+		public static void Main (string[] args)
+		{
+			try {
+				ReadNumber (MinimalNumber, MaximalNumber);
+
+				Console.Write ("{0} < ", MinimalNumber);
+				Console.Write (string.Join (" < ", Numbers));
+				Console.Write (" < {0}", MaximalNumber);
+				Console.WriteLine ();
+
+			} catch {
+				Console.WriteLine ("Exception");
+			}
+		}
+
+		private static void ReadNumber (int start, int end)
+		{
+			int lastNumber = start;
+			for (int i = 0; i < NumberOfExpectedNumbers; ++i) {
+				string input = Console.ReadLine ();
+				int number;
+				if (int.TryParse (input, out number) && (number > lastNumber) && (number < end)) {
+					Numbers.Add (number);
+					lastNumber = number;
+				} else {
+					throw new ArgumentException ();
+				}
+			}
+		}
+	}
+}
---
+++ CSharp-2 - 2016/Homeworks/07-Exception-Handling/07-Exception-Hadling/SquareRoot/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SquareRoot
+{
+	using System;
+
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			try {
+				string input = Console.ReadLine ();
+				double number = double.Parse (input);
+
+				if (number < 0) {
+
+					throw new ArgumentException();
+
+				}
+
+				Console.WriteLine ("{0:f3}", Math.Sqrt (number));
+			
+			} catch {
+			
+				Console.WriteLine ("Invalid number");
+			
+			} finally {
+			
+				Console.WriteLine ("Good bye");
+			
+			}
+		}
+	}
+}
