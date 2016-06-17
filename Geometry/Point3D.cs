69afd74c96c5ebc6e236a3739d533c5925b41490
bozhink <bozhink@gmail.com>
2016-06-17 10:01:17 +0300

    DistanceCalculator is added.

--- Geometry/Point3D.cs
+++ Geometry/Point3D.cs
@@ -2,14 +2,14 @@
 {
     public struct Point3D
     {
-        private static readonly Point3D origin = new Point3D
+        private static readonly Point3D Origin = new Point3D
         {
             X = 0.0,
             Y = 0.0,
             Z = 0.0
         };
 
-        public static Point3D O => origin;
+        public static Point3D O => Origin;
 
         public double X { get; set; }
 
