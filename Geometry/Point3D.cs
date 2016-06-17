f06f600264e9d015d270fa063c95c4e052fcc41d
bozhink <bozhink@gmail.com>
2016-06-17 09:49:44 +0300

    Point3D is added.

---
+++ Geometry/Point3D.cs
@@ -0,0 +1,16 @@
+﻿namespace Geometry
+{
+    public struct Point3D
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public override string ToString()
+        {
+            return $"({this.X}, {this.Y}, {this.Z})";
+        }
+    }
+}
