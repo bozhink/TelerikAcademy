07b018142a1294283198cd4dc5b567a44eeabb73
bozhink <bozhink@gmail.com>
2016-06-17 13:40:38 +0300

    Version.Extensions are added.

---
+++ Version/Extensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Version
+{
+    using System;
+    using System.Linq;
+
+    public static class Extensions
+    {
+        public static string GetVersion(this Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            var version = type.GetCustomAttributes(false).FirstOrDefault(a => a.GetType() == typeof(VersionAttribute)) as VersionAttribute;
+
+            return version.ToString();
+        }
+
+        public static string GetVersion<T>(this T obj)
+        {
+            return GetVersion(typeof(T));
+        }
+    }
+}
