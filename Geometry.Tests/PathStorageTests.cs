18aea554cef696135db37ee679ef1c5b3075a703
bozhink <bozhink@gmail.com>
2016-06-17 10:37:55 +0300

    PathStorageTests are added.

---
+++ Geometry.Tests/PathStorageTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Geometry.Tests
+{
+    using System;
+
+    public class PathStorageTests
+    {
+        private const int NumberOfPoints = 10;
+        private const string PathFileName = "test-path-file-name.txt";
+
+        public void WriteAndReadPathTest()
+        {
+            var path = new Path();
+
+            for (int i = 1; i < NumberOfPoints; ++i)
+            {
+                var point = new Point3D
+                {
+                    X = i,
+                    Y = i,
+                    Z = i
+                };
+
+                path.SequenceOfPoints.Add(point);
+            }
+
+            Console.WriteLine(string.Join(", ", path.SequenceOfPoints));
+            Console.WriteLine();
+
+            PathStorage.SavePathToFile(PathFileName, path);
+
+            var newPath = PathStorage.LoadPathFromFile(PathFileName);
+            Console.WriteLine(string.Join(", ", newPath.SequenceOfPoints));
+        }
+    }
+}
---
+++ Geometry.Tests/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Geometry.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pathStorageTests = new PathStorageTests();
+            pathStorageTests.WriteAndReadPathTest();
+        }
+    }
+}
