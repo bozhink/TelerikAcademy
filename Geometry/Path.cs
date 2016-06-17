cecded30df5814f17f4470f1fa95760df7722adf
bozhink <bozhink@gmail.com>
2016-06-17 10:08:00 +0300

    Path is added.

---
+++ Geometry/Path.cs
@@ -0,0 +1,30 @@
+﻿namespace Geometry
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Path
+    {
+        public Path()
+        {
+            this.SequenceOfPoints = new List<Point3D>();
+        }
+
+        public Path(params Point3D[] points)
+        {
+            if (points == null)
+            {
+                throw new ArgumentNullException(nameof(points));
+            }
+
+            if (points.Length < 1)
+            {
+                throw new ArgumentException(nameof(points));
+            }
+
+            this.SequenceOfPoints = new List<Point3D>(points);
+        }
+
+        public ICollection<Point3D> SequenceOfPoints { get; private set; }
+    }
+}
