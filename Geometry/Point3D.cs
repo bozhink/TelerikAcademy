0ff382141d22c93140801202393eb5a8d778e7d6
bozhink <bozhink@gmail.com>
2016-06-17 09:55:10 +0300

    Origin point is added to Point3D.

--- Geometry/Point3D.cs
+++ Geometry/Point3D.cs
@@ -2,6 +2,15 @@
 {
     public struct Point3D
     {
+        private static readonly Point3D origin = new Point3D
+        {
+            X = 0.0,
+            Y = 0.0,
+            Z = 0.0
+        };
+
+        public static Point3D O => origin;
+
         public double X { get; set; }
 
         public double Y { get; set; }
