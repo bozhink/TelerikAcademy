e85dcc761165433fd9a44d3cec52eb337a67e603
bozhink <bozhink@gmail.com>
2016-06-17 13:07:36 +0300

    VersionAttribute is added.

---
+++ Version/VersionAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Version
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Enum)]
+    public class VersionAttribute : Attribute
+    {
+        public VersionAttribute(int major, int minor)
+        {
+            this.Major = major;
+            this.Minor = minor;
+        }
+
+        public int Major { get; private set; }
+
+        public int Minor { get; private set; }
+
+        public override string ToString()
+        {
+            return $"Version {this.Major}.{this.Minor}";
+        }
+    }
+}
