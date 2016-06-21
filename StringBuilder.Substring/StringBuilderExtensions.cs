bd82f489d04e255f753c43dbbb2f3867299ce355
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-21 21:52:02 +0300

    StringBuilder.Substring is added.

---
+++ StringBuilder.Substring/StringBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace StringBuilder.Substring
+{
+    using System.Text;
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder Substring(this StringBuilder stringBuilder, int index)
+        {
+            return new StringBuilder(stringBuilder?.ToString().Substring(index));
+        }
+
+        public static StringBuilder Substring(this StringBuilder stringBuilder, int index, int length)
+        {
+            return new StringBuilder(stringBuilder?.ToString().Substring(index, length));
+        }
+    }
+}
