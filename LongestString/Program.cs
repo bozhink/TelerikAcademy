3ff675c1833e55aff7f800edcdf40e75aac1ec44
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-22 22:58:43 +0300

    LongestString is implemented.

---
+++ LongestString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LongestString
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: {0} <string1>[ <string2>, ...]", AppDomain.CurrentDomain.FriendlyName);
+                Environment.Exit(1);
+            }
+
+            string longestString = args.FirstOrDefault(x => x.Length == args.Max(y => y.Length));
+            Console.WriteLine(longestString);
+        }
+    }
+}
