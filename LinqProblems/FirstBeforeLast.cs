4acb0db500c7830b60047074c85d7f62400ac355
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-22 21:33:20 +0300

    FirstBeforeLast is implemented.

---
+++ LinqProblems/FirstBeforeLast.cs
@@ -0,0 +1,20 @@
+﻿namespace LinqProblems
+{
+    using System;
+    using System.Linq;
+    using Models;
+
+    public class FirstBeforeLast
+    {
+        public Student[] GetStudentsWithFirstBeforeLastName(Student[] students)
+        {
+            if (students == null || students.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(students));
+            }
+
+            return students.Where(s => string.Compare(s.FirstName, s.LastName, true) < 0)
+                .ToArray();
+        }
+    }
+}
---
+++ LinqProblems/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqProblems.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public uint StudentId { get; set; }
+    }
+}
