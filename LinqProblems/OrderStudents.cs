fd81eafb34c22f87eb1dd1d35945223fc760ce92
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-22 21:46:12 +0300

    OrderStudents is implemented.

---
+++ LinqProblems/OrderStudents.cs
@@ -0,0 +1,21 @@
+﻿namespace LinqProblems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    public class OrderStudents
+    {
+        public IEnumerable<Student> OrderStudentsByFirstThenByLastName(IEnumerable<Student> students)
+        {
+            if (students == null)
+            {
+                throw new ArgumentNullException(nameof(students));
+            }
+
+            return students.OrderBy(s => s.FirstName)
+                .ThenByDescending(s => s.LastName);
+        }
+    }
+}
