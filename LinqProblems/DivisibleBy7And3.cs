86a6464937b60a68dd9036809fcdfb4f3960a1b9
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-22 21:51:38 +0300

    DivisibleBy7And3 is implemented.

---
+++ LinqProblems/DivisibleBy7And3.cs
@@ -0,0 +1,20 @@
+﻿namespace LinqProblems
+{
+    using System;
+    using System.Linq;
+
+    public class DivisibleBy7And3
+    {
+        public void PrintDivisibleBy7And3Numbers(params int[] numbers)
+        {
+            if (numbers == null || numbers.Length < 1)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            numbers.Where(n => (n % 7 == 0) && (n % 3 == 0))
+                .ToList()
+                .ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
