1bdda24ae3886c970c58305755830ad55158aa73
Bozhin Karaivanov <bozhink@gmail.com>
2016-06-22 21:57:55 +0300

    Timer is implemented.

---
+++ Timer/Timer.cs
@@ -0,0 +1,40 @@
+﻿namespace Timer
+{
+    using System;
+    using System.Diagnostics;
+
+    public class Timer
+    {
+        private readonly int seconds;
+
+        public Timer(int seconds)
+        {
+            this.seconds = seconds;
+        }
+
+        public delegate void PrintSomething();
+
+        public void InvokeDelegate()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var printDelegate = new PrintSomething(PrintTime);
+
+            while (true)
+            {
+                if (stopwatch.Elapsed.Seconds != this.seconds)
+                {
+                    continue;
+                }
+
+                printDelegate.Invoke();
+                stopwatch.Restart();
+            }
+        }
+
+        private static void PrintTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
