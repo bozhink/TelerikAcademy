b115a3340c8b8bd081d2c0b70a9809d040ea96b1
Bozhin Karaivanov <bozhink@gmail.com>
2016-07-25 18:52:14 +0300

    JavaScript-UI-and-DOM-2016\07. Event Model\homework is done.

--- JavaScript-UI-and-DOM-2016/07. Event Model/homework/tasks/task-1.js
+++ JavaScript-UI-and-DOM-2016/07. Event Model/homework/tasks/task-1.js
@@ -2,14 +2,55 @@
 
 /* 
 
-Create a function that takes an id or DOM element and:
+Create a function that takes an id or DOM selector and:
   
 
 */
 
-function solve(){
-  return function (selector) {
-  };
-};
+function solve() {
+    'use strict';
+    function toggle(event) {
+        var content, button;
+        if (event.target.className === 'button') {
+            for (content = button = event.target; content != null && content.className !== 'content'; content = content.nextElementSibling) {}
+
+            if (content.style.display === '') {
+                button.innerHTML = 'show';
+                content.style.display = 'none';
+            } else if (content.style.display === 'none') {
+                button.innerHTML = 'hide';
+                content.style.display = '';
+            }
+        }
+    }
+    
+	return function (selector) {
+		var id, i, len, myElement,
+            buttons, root;
+
+		if (selector == null || typeof (selector) !== 'string') {
+			throw new Error('Invalid selector.');
+		}
+
+		myElement = selector;
+		if (!(myElement instanceof HTMLElement)) {
+			id = (selector[0] === "#") ? selector.substr(1) : selector;
+			myElement = document.getElementById(id);
+			if (myElement == null) {
+				throw new Error('No such an element ' + selector);
+			}
+		}
+
+        buttons = document.querySelectorAll('.button');
+        for (i = 0, len = buttons.length; i < len; i += 1) {
+            buttons[i].innerHTML = 'hide';
+        }
+
+        root = document.getElementById('root');
+        if (root != null) {
+            root.addEventListener('click', toggle, false);
+        }
+    };
+}
 
 module.exports = solve;
