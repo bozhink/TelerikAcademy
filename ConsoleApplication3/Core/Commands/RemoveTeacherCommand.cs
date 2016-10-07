f68883553d144a5e2a82c0394216aea0260012ed
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-07 13:30:29 +0300

    RemoveTeacherCommand is implemented

---
+++ ConsoleApplication3/Core/Commands/RemoveTeacherCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace SchoolSystem.Core.Commands
+{
+    using System.Collections.Generic;
+    using Contracts;
+
+    internal class RemoveTeacherCommand : ICommand
+    {
+        // RemoveTeacher [TeacherId]
+        public string Execute(IList<string> parameters)
+        {
+            var teacherId = int.Parse(parameters[0]);
+
+            Engine.Teachers.Remove(teacherId);
+
+            return $"Teacher with ID {teacherId} was successfully removed.";
+        }
+    }
+}
