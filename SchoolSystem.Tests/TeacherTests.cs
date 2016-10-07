6b10d12a69e02ed05b6d2377f9e7e1d4c370c9b1
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-07 14:58:56 +0300

    TeacherTests is added.

---
+++ SchoolSystem.Tests/TeacherTests.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolSystem.Tests
+{
+    using System;
+    using NUnit.Framework;
+    using SchoolSystem.Contracts;
+    using SchoolSystem.Models;
+    using SchoolSystem.Types;
+
+    [TestFixture]
+    public class TeacherTests
+    {
+    }
+}
