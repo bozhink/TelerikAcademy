a63e50a91d8b8e0343a128ea76d9244d9b84849a
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-07 14:36:17 +0300

    The middleman BusinessLogicService is removed.

--- ConsoleApplication3/Startup.cs
+++ ConsoleApplication3/Startup.cs
@@ -1,17 +1,16 @@
 ﻿namespace SchoolSystem
 {
-    using SchoolSystem.Core;
+    using Core;
 
     public class Startup
     {
         public static void Main()
         {
-            // TODO: abstract at least 2 more provider like this one
             var reader = new ConsoleReader();
             var writer = new ConsoleWriter();
 
-            var service = new BusinessLogicService();
-            service.Execute(reader, writer);
+            var engine = new Engine(reader, writer);
+            engine.Run();
         }
     }
 }
