db40c11e88624d12520db4676e2bd06b6fa87a6e
Bozhin Karaivanov <bozhink@gmail.com>
2016-10-07 13:07:31 +0300

    Grade: 1 to 12

--- ConsoleApplication3/Types/Grade.cs
+++ ConsoleApplication3/Types/Grade.cs
@@ -2,6 +2,13 @@
 {
     public enum Grade
     {
+        First = 1,
+        Second = 2,
+        Third = 3,
+        Fourth = 4,
+        Fift = 5,
+        Sixth = 6,
+        Seventh = 7,
         Eighth = 8,
         Ninth = 9,
         Tenth = 10,
