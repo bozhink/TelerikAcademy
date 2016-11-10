c3f760861a7f1359e74f7bd7a95816a33f243e0d
bozhink <bozhink@gmail.com>
2016-11-10 10:16:55 +0200

    Dealership.Constants.Messages is moved to Dealership.Common.Messages

---
+++ Dealership.Common/Messages.cs
@@ -0,0 +1,29 @@
+﻿namespace Dealership.Common
+{
+    public static class Messages
+    {
+        public const string CommentAddedSuccessfully = "{0} added comment successfully!";
+
+        public const string CommentDoesNotExist = "The comment does not exist!";
+
+        public const string CommentRemovedSuccessfully = "{0} removed comment successfully!";
+
+        // Commands constants
+        public const string InvalidCommand = "Invalid command!";
+
+        public const string NoSuchUser = "There is no user with username {0}!";
+        public const string RemovedCommentDoesNotExist = "Cannot remove comment! The comment does not exist!";
+        public const string RemovedVehicleDoesNotExist = "Cannot remove comment! The vehicle does not exist!";
+        public const string UserAlreadyExist = "User {0} already exist. Choose a different username!";
+        public const string UserLoggedIn = "User {0} successfully logged in!";
+        public const string UserLoggedInAlready = "User {0} is logged in! Please log out first!";
+        public const string UserLoggedOut = "You logged out!";
+        public const string UserNotLogged = "You are not logged! Please login first!";
+        public const string UserRegisterеd = "User {0} registered successfully!";
+        public const string VehicleAddedSuccessfully = "{0} added vehicle successfully!";
+        public const string VehicleDoesNotExist = "The vehicle does not exist!";
+        public const string VehicleRemovedSuccessfully = "{0} removed vehicle successfully!";
+        public const string WrongUsernameOrPassword = "Wrong username or password!";
+        public const string YouAreNotAnAdmin = "You are not an admin!";
+    }
+}
