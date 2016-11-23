6fe61985725802e82d3b3bc0d0ba66952f197511
bozhink <bozhink@gmail.com>
2016-11-23 15:35:56 +0200

    ReverseSequenceWithStack is added.

---
+++ ReverseSequenceWithStack/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace ReverseSequenceWithStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var sequence = new Stack<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                int item;
+                if (!int.TryParse(input, out item))
+                {
+                    Console.WriteLine("Input should be integer number but is '{0}'.", input);
+                    continue;
+                }
+
+                sequence.Push(item);
+            }
+
+            while (sequence.Count > 0)
+            {
+                var item = sequence.Pop();
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
