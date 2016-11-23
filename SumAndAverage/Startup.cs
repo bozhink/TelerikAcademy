45f2d31ba28c8f9a66fbf254f625a43e8f42e678
bozhink <bozhink@gmail.com>
2016-11-23 15:31:11 +0200

    SumAndAverage is added

---
+++ SumAndAverage/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace SumAndAverage
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var inputList = new List<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                int item;
+                if (!int.TryParse(input, out item) || item < 1)
+                {
+                    Console.WriteLine("Input should be positive integer number but is '{0}'.", input);
+                    continue;
+                }
+
+                inputList.Add(item);
+            }
+
+            long sum = 0L;
+            double average = 0.0;
+            foreach (var item in inputList)
+            {
+                sum += item;
+            }
+
+            average = (1.0 * sum) / inputList.Count;
+
+            Console.WriteLine("Sum = {0}, Average = {1}", sum, average);
+        }
+    }
+}
