123a145e983745d0fb8b3ef28e7c81447396e61b
bozhink <bozhink@gmail.com>
2016-11-23 16:27:58 +0200

    OddRemover is added

---
+++ OddRemover/Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace OddRemover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        // {4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2} → {5, 3, 3, 5}
+        public static void Main(string[] args)
+        {
+            var sequence = new List<int> { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+            Console.WriteLine(string.Join(", ", sequence));
+            RemoveOddOcurrences(sequence);
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+
+        public static void RemoveOddOcurrences(IList<int> sequence)
+        {
+            var differentItems = sequence.Distinct().ToArray();
+            var numberOfOccurences = new int[differentItems.Length];
+
+            for (int i = 0; i < differentItems.Length; i++)
+            {
+                var item = differentItems[i];
+                numberOfOccurences[i] = sequence.Count(x => x == item);
+            }
+
+            for (int i = 0; i < numberOfOccurences.Length; i++)
+            {
+                if (numberOfOccurences[i] % 2 == 1)
+                {
+                    var itemToRemove = differentItems[i];
+
+                    while (sequence.Remove(itemToRemove))
+                    {
+                        // Empty
+                    }
+                }
+            }
+        }
+    }
+}
