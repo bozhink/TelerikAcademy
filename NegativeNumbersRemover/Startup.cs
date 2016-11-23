cb2f7ad4e0c4dc068b654772aa5d8fbdc183897a
bozhink <bozhink@gmail.com>
2016-11-23 16:13:12 +0200

    NegativeNumbersRemover is added.

---
+++ NegativeNumbersRemover/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace NegativeNumbersRemover
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var sequence = new List<int> { 1, 2, -123, 12, -3, 12, -3, 12, 31, -32, 12, 3, 123, 1, 23, -12, 3, 123, -12 };
+            RemoveNegativeNumbers(sequence);
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+
+        public static void RemoveNegativeNumbers(IList<int> sequence)
+        {
+            for (int i = 0; i < sequence.Count;)
+            {
+                var currentItem = sequence[i];
+                if (currentItem < 0)
+                {
+                    sequence.Remove(currentItem);
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+    }
+}
