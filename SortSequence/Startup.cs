765a05dc89ad7f627fd30516d21acbc5c52c8b9a
bozhink <bozhink@gmail.com>
2016-11-23 15:40:40 +0200

    SortSequence is added.

---
+++ SortSequence/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace SortSequence
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var sequence = new List<int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                int item;
+                if (!int.TryParse(input, out item))
+                {
+                    Console.WriteLine("Input should be integer number but is '{0}'.", input);
+                    continue;
+                }
+
+                sequence.Add(item);
+            }
+
+            sequence.Sort();
+
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+    }
+}
